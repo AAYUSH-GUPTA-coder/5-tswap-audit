62c9a0b0b42d28b03b01f3de808fd6b1c455fe25
Aayush Gupta <guptaaayush432@gmail.com>
2024-07-28 21:26:32 +0530

    corrected import path

--- test/invariant/Invariant.t.sol
+++ test/invariant/Invariant.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {PoolFactory} from "../../src/PoolFactory.sol";
 import {TSwapPool} from "../../src/TSwapPool.sol";
 import {Handler} from "./Handler.t.sol";
